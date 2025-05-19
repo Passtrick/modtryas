1ece427ab6c4938e4cb409d35cd31ff11ce3e18f
HuoyerSohel <pepezzm10s@gmail.com>
2025-05-18 20:57:13 -0500

    Actualizar NepCaller.java

--- app/src/main/java/uk/lgl/modmenu/NepCaller.java
+++ app/src/main/java/uk/lgl/modmenu/NepCaller.java
@@ -2,19 +2,24 @@ package uk.lgl.modmenu;
 
 import android.app.Application;
 import com.topjohnwu.superuser.Shell;
+import uk.lgl.BuildConfig;
 
 public class NepCaller extends Application {
+
+    static {
+        // Configura el shell antes de que se cree la clase
+        Shell.enableVerboseLogging = BuildConfig.DEBUG;
+        Shell.setDefaultBuilder(Shell.Builder.create()
+            .setFlags(Shell.FLAG_REDIRECT_STDERR)
+            .setTimeout(10));
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // Configura el shell
-        Shell.Config.setFlags(Shell.FLAG_REDIRECT_STDERR);
-        Shell.Config.verboseLogging(BuildConfig.DEBUG);
-
+        
         // Inicializa Shell en el hilo principal
         Shell.getShell(shell -> {
-            // Ejecuta comandos root
             shell.newJob().add("setenforce 0").submit();
         });
     }
