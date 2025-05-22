a2cabc98061920a13fc366e46f35d5b0aee5612d
HuoyerSohel <pepezzm10s@gmail.com>
2025-05-21 22:34:44 -0500

    Actualizar Setup.h

--- app/src/main/jni/Menu/Setup.h
+++ app/src/main/jni/Menu/Setup.h
@@ -1,9 +1,11 @@
+#ifndef MODMENU_SETUP_H
+#define MODMENU_SETUP_H
+
 #include <sstream>
 #include "Menu/Menu.h"
 #include "Menu/get_device_api_level_inlines.h"
 
-//Jni stuff from MrDarkRX https://github.com/MrDarkRXx/DarkMod-Floating
-void setDialog(jobject ctx, JNIEnv *env, const char *title, const char *msg){
+void setDialog(jobject ctx, JNIEnv *env, const char *title, const char *msg) {
     jclass Alert = env->FindClass(OBFUSCATE("android/app/AlertDialog$Builder"));
     jmethodID AlertCons = env->GetMethodID(Alert, OBFUSCATE("<init>"), OBFUSCATE("(Landroid/content/Context;)V"));
 
@@ -33,17 +35,17 @@ void setDialog(jobject ctx, JNIEnv *env, const char *title, const char *msg){
 void Toast(JNIEnv *env, jobject thiz, const char *text, int length) {
     jstring jstr = env->NewStringUTF(text);
     jclass toast = env->FindClass(OBFUSCATE("android/widget/Toast"));
-    jmethodID methodMakeText =env->GetStaticMethodID(toast,OBFUSCATE("makeText"),OBFUSCATE("(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;"));
-    jobject toastobj = env->CallStaticObjectMethod(toast, methodMakeText,thiz, jstr, length);
+    jmethodID methodMakeText = env->GetStaticMethodID(toast, OBFUSCATE("makeText"), OBFUSCATE("(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;"));
+    jobject toastobj = env->CallStaticObjectMethod(toast, methodMakeText, thiz, jstr, length);
     jmethodID methodShow = env->GetMethodID(toast, OBFUSCATE("show"), OBFUSCATE("()V"));
     env->CallVoidMethod(toastobj, methodShow);
 }
 
-void startActivityPermisson(JNIEnv *env, jobject ctx){
+void startActivityPermisson(JNIEnv *env, jobject ctx) {
     jclass native_context = env->GetObjectClass(ctx);
-    jmethodID startActivity = env->GetMethodID(native_context, OBFUSCATE("startActivity"),OBFUSCATE("(Landroid/content/Intent;)V"));
+    jmethodID startActivity = env->GetMethodID(native_context, OBFUSCATE("startActivity"), OBFUSCATE("(Landroid/content/Intent;)V"));
 
-    jmethodID pack = env->GetMethodID(native_context, OBFUSCATE("getPackageName"),OBFUSCATE("()Ljava/lang/String;"));
+    jmethodID pack = env->GetMethodID(native_context, OBFUSCATE("getPackageName"), OBFUSCATE("()Ljava/lang/String;"));
     jstring packageName = static_cast<jstring>(env->CallObjectMethod(ctx, pack));
 
     const char *pkg = env->GetStringUTFChars(packageName, 0);
@@ -59,17 +61,17 @@ void startActivityPermisson(JNIEnv *env, jobject ctx){
 
     jclass intentclass = env->FindClass(OBFUSCATE("android/content/Intent"));
     jmethodID newIntent = env->GetMethodID(intentclass, OBFUSCATE("<init>"), OBFUSCATE("(Ljava/lang/String;Landroid/net/Uri;)V"));
-    jobject intent = env->NewObject(intentclass,newIntent,env->NewStringUTF(OBFUSCATE("android.settings.action.MANAGE_OVERLAY_PERMISSION")), UriMethod);
+    jobject intent = env->NewObject(intentclass, newIntent, env->NewStringUTF(OBFUSCATE("android.settings.action.MANAGE_OVERLAY_PERMISSION")), UriMethod);
 
     env->CallVoidMethod(ctx, startActivity, intent);
 }
 
-void startService(JNIEnv *env, jobject ctx){
+void startService(JNIEnv *env, jobject ctx) {
     jclass native_context = env->GetObjectClass(ctx);
     jclass intentClass = env->FindClass(OBFUSCATE("android/content/Intent"));
     jclass actionString = env->FindClass(OBFUSCATE("com/android/support/Launcher"));
     jmethodID newIntent = env->GetMethodID(intentClass, OBFUSCATE("<init>"), OBFUSCATE("(Landroid/content/Context;Ljava/lang/Class;)V"));
-    jobject intent = env->NewObject(intentClass,newIntent,ctx,actionString);
+    jobject intent = env->NewObject(intentClass, newIntent, ctx, actionString);
     jmethodID startActivityMethodId = env->GetMethodID(native_context, OBFUSCATE("startService"), OBFUSCATE("(Landroid/content/Intent;)Landroid/content/ComponentName;"));
     env->CallObjectMethod(ctx, startActivityMethodId, intent);
 }
@@ -79,19 +81,16 @@ void *exit_thread(void *) {
     exit(0);
 }
 
-//Needed jclass parameter because this is a static java method
-void CheckOverlayPermission(JNIEnv *env, jclass thiz, jobject ctx){
-    //If overlay permission option is greyed out, make sure to add android.permission.SYSTEM_ALERT_WINDOW in manifest
-
-    LOGI(OBFUSCATE("Check overlay permission"));
+void CheckOverlayPermission(JNIEnv *env, jclass thiz, jobject ctx) {
+    LOGI("%s", OBFUSCATE("Check overlay permission").get_str());
 
     int sdkVer = api_level();
-    if (sdkVer >= 23){ //Android 6.0
+    if (sdkVer >= 23) {
         jclass Settings = env->FindClass(OBFUSCATE("android/provider/Settings"));
-        jmethodID canDraw =env->GetStaticMethodID(Settings, OBFUSCATE("canDrawOverlays"), OBFUSCATE("(Landroid/content/Context;)Z"));
-        if (!env->CallStaticBooleanMethod(Settings, canDraw, ctx)){
-            Toast(env,ctx,OBFUSCATE("Overlay permission is required in order to show mod menu."),1);
-            Toast(env,ctx,OBFUSCATE("Overlay permission is required in order to show mod menu."),1);
+        jmethodID canDraw = env->GetStaticMethodID(Settings, OBFUSCATE("canDrawOverlays"), OBFUSCATE("(Landroid/content/Context;)Z"));
+        if (!env->CallStaticBooleanMethod(Settings, canDraw, ctx)) {
+            Toast(env, ctx, OBFUSCATE("Overlay permission is required in order to show mod menu."), 1);
+            Toast(env, ctx, OBFUSCATE("Overlay permission is required in order to show mod menu."), 1);
             startActivityPermisson(env, ctx);
 
             pthread_t ptid;
@@ -100,28 +99,29 @@ void CheckOverlayPermission(JNIEnv *env, jclass thiz, jobject ctx){
         }
     }
 
-
-    LOGI(OBFUSCATE("Start service"));
-
-    //StartMod Normal
+    LOGI("%s", OBFUSCATE("Start service").get_str());
     startService(env, ctx);
 }
 
-void Init(JNIEnv *env, jobject thiz, jobject ctx, jobject title, jobject subtitle){
-    //Set sub title
+void Init(JNIEnv *env, jobject thiz, jobject ctx, jobject title, jobject subtitle) {
     setText(env, title, OBFUSCATE("<b>Modded by (yourname)</b>"));
 
-    //Set sub title
     setText(env, subtitle, OBFUSCATE("<b><marquee><p style=\"font-size:30\">"
-                                     "<p style=\"color:green;\">Modded by LGL</p> | "
-                                     "https://github.com/LGLTeam | Lorem Ipsum is simply dummy text of the printing and typesetting</p>"
-                                     "</marquee></b>"));
+                                    "<p style=\"color:green;\">Modded by LGL</p> | "
+                                    "https://github.com/LGLTeam | Lorem Ipsum is simply dummy text of the printing and typesetting</p>"
+                                    "</marquee></b>"));
 
-    //Dialog Example
-    //setDialog(ctx,env,OBFUSCATE("Title"),OBFUSCATE("Message Example"));
-
-    //Toast Example
-    Toast(env,ctx,OBFUSCATE("Modded by YOU"),ToastLength::LENGTH_LONG);
+    Toast(env, ctx, OBFUSCATE("Modded by YOU"), ToastLength::LENGTH_LONG);
 
     initValid = true;
 }
+
+const char *Icon() {
+    return "";  // Espacio para tu imagen en base64
+}
+
+const char *IconWebViewData() {
+    return "";  // Espacio para tus datos web
+}
+
+#endif //MODMENU_SETUP_H
