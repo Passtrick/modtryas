0963db62b083da321e89850540053a3fa94d8c6a
HuoyerSohel <pepezzm10s@gmail.com>
2025-05-23 13:54:34 -0500

    Actualizar hack.cpp

--- app/src/main/cpp/hack.cpp
+++ app/src/main/cpp/hack.cpp
@@ -422,17 +422,3 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
 }
 #endif
 
------ ARCHIVO: hack.h -----
-
-//
-// Created by Perfare on 2020/7/4.
-//
-
-#ifndef ZYGISK_IL2CPPDUMPER_HACK_H
-#define ZYGISK_IL2CPPDUMPER_HACK_H
-
-#include <stddef.h>
-
-void hack_prepare(const char *game_data_dir, void *data, size_t length);
-
-#endif //ZYGISK_IL2CPPDUMPER_HACK_H
