6e3382b96163c958416f404de85aa94195831cda
HuoyerSohel <pepezzm10s@gmail.com>
2025-05-23 14:59:49 -0500

    Actualizar main.cpp

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,6 +1,5 @@
 #include <jni.h>
 #include <string>
-#include <unistd.h>
 #include "hack.h"
 #include "log.h"
 #include <thread>
@@ -9,32 +8,25 @@ extern "C" JNIEXPORT void JNICALL
 Java_com_perfare_il2cppdumper_MainActivity_dumpIl2Cpp(
     JNIEnv* env,
     jobject /* this */,
-    jstring packageName,
     jstring dataDir) {
     
     const char* dataDirStr = env->GetStringUTFChars(dataDir, nullptr);
     if (!dataDirStr) {
-        LOGE("Failed to get data directory string");
+        LOGE("Data directory string is null");
         return;
     }
+
+    LOGI("Starting dump for: %s", dataDirStr);
     
-    LOGI("Starting dump for data directory: %s", dataDirStr);
-    
-    // Verificar root (opcional, solo para logging)
-    if (getuid() != 0) {
-        LOGW("Not running as root, some features may not work");
-    }
-    
-    // Iniciar el dump en un hilo separado
-    std::thread dump_thread([dataDirStr]() {
+    // Iniciar en un hilo separado
+    std::thread([dataDirStr]() {
         hack_prepare(dataDirStr);
-    });
-    dump_thread.detach();
+    }).detach();
     
     env->ReleaseStringUTFChars(dataDir, dataDirStr);
 }
 
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    LOGI("IL2CPP Dumper APK loaded");
+    LOGI("IL2CPP Dumper initialized");
     return JNI_VERSION_1_6;
 }
