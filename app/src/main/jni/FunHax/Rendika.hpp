eb29d0f9eaa44f990ccfdad8762748fd72c05cab
HuoyerSohel <pepezzm10s@gmail.com>
2025-06-02 04:33:23 -0500

    Actualizar Rendika.hpp

--- app/src/main/jni/FunHax/Rendika.hpp
+++ app/src/main/jni/FunHax/Rendika.hpp
@@ -6,23 +6,28 @@
 
 // ========== SISTEMA DECLARE_CLASS ==========
 #define DECLARE_CLASS(className) \
-struct className { \
-    static uintptr_t ResolveMethod(const char* methodName, int argsCount = 0, const char* assembly = "Assembly-CSharp", const char* namespaze = "") { \
-        void* method = Il2CppGetMethodOffset(assembly, namespaze, #className, methodName, argsCount); \
-        if (!method && namespaze[0] != '\0') { \
-            method = Il2CppGetMethodOffset(assembly, "", #className, methodName, argsCount); \
-        } \
-        if (!method) { \
-            LOGD("Método %s no encontrado en %s.%s", methodName, namespaze, #className); \
-        } \
-        return (uintptr_t)method; \
+namespace Class { \
+    struct className { \
+        static uintptr_t ResolveMethod(const char* methodName, int argsCount = -1, const char* assembly = "Assembly-CSharp", const char* namespaze = ""); \
+        static uintptr_t ResolveProperty(const char* propName, bool isGetter = true, const char* assembly = "Assembly-CSharp", const char* namespaze = ""); \
+    }; \
+} \
+uintptr_t Class::className::ResolveMethod(const char* methodName, int argsCount, const char* assembly, const char* namespaze) { \
+    void* method = Il2CppGetMethodOffset(assembly, namespaze, #className, methodName, argsCount); \
+    if (!method && namespaze[0] != '\0') { \
+        method = Il2CppGetMethodOffset(assembly, "", #className, methodName, argsCount); \
     } \
-    static uintptr_t ResolveProperty(const char* propName, bool isGetter = true, const char* assembly = "Assembly-CSharp", const char* namespaze = "") { \
-        const char* prefix = isGetter ? "get_" : "set_"; \
-        char fullName[256]; \
-        snprintf(fullName, sizeof(fullName), "%s%s", prefix, propName); \
-        return ResolveMethod(fullName, isGetter ? 0 : 1, assembly, namespaze); \
-    }
+    if (!method) { \
+        LOGD("Método %s no encontrado en %s.%s", methodName, namespaze, #className); \
+    } \
+    return (uintptr_t)method; \
+} \
+uintptr_t Class::className::ResolveProperty(const char* propName, bool isGetter, const char* assembly, const char* namespaze) { \
+    const char* prefix = isGetter ? "get_" : "set_"; \
+    char fullName[256]; \
+    snprintf(fullName, sizeof(fullName), "%s%s", prefix, propName); \
+    return ResolveMethod(fullName, isGetter ? 0 : 1, assembly, namespaze); \
+}
 
 #define END_DECLARE() };
 
