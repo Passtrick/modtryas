9ccefc9005bbc907f12518d7cebefe920e59bb2a
HuoyerSohel <pepezzm10s@gmail.com>
2025-06-02 06:55:35 -0500

    Actualizar Rendika.hpp

--- app/src/main/jni/FunHax/Rendika.hpp
+++ app/src/main/jni/FunHax/Rendika.hpp
@@ -1,14 +1,14 @@
 #pragma once
-
-//
 // Created By RendikaRahardian | TG : FunHax
 // 26 - Apr - 2024
-//
 
-int (*old_GetTotalXp)(void *instance);
-int GetTotalXp(void *instance) {
-    if (instance != NULL) {
-        return 150000; // High XP value over 100k
+namespace FunHax {
+    inline int (*old_GetTotalXp)(void* instance) = nullptr;  // Inicializado como nullptr
+    
+    inline int GetTotalXp(void* instance) {
+        if (instance != nullptr) {
+            return 150000; // High XP value
+        }
+        return old_GetTotalXp(instance); // Llama a la original si no hay instancia
     }
-    return old_GetTotalXp(instance);
 }
