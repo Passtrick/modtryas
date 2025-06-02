aff13cfece8a0d0ded86ce61ac895bd6919bdee5
HuoyerSohel <pepezzm10s@gmail.com>
2025-06-02 07:12:51 -0500

    Actualizar Rendika.hpp

--- app/src/main/jni/FunHax/Rendika.hpp
+++ app/src/main/jni/FunHax/Rendika.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
 namespace FunHax {
-    // Declaramos como extern para que no sea inline
     extern int (*old_GetTotalXp)(void* instance);
     
-    // La función hook puede ser inline
     inline int GetTotalXp(void* instance) {
         if (instance != nullptr) {
             return 150000;
